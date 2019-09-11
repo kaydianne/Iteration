0cf9b57fc857880af3e5d294e5fc2e41171257f9
Kay <Kaydi@DESKTOP-U7ER47O>
2019-09-11 18:55:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer value between 1 and 100: ");
+
+            try
+            {
+                string input = Console.ReadLine();
+
+                int number = int.Parse(input); ;
+
+                if ((number > 0) && (number <=100))
+                {
+                    for (int i = 0; i < number; i++)
+                    {
+                        Console.WriteLine("You have entered " + number +
+                         ". This is the current integer value in the loop: " + i.ToString());
+                       
+                    }
+
+                    if (number%2 ==0)
+                    {
+                        Console.WriteLine("The number you entered is even!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number you entered is odd!");
+                    }
+
+                    Console.WriteLine("Press any key to exit the program...");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an integer between 1 and 100.");
+                    Console.WriteLine("press any key to exit the program and try again...");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer between 1 and 100");
+                Console.WriteLine("Press any key to exit and try again");
+            }
+        }
+    }
+}
